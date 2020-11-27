6436cfc65856887ae5e107aded346d16742bc1c1
thatguywiththatname <simjenner3@gmail.com>
2020-11-27 02:04:26 +0000

    Working countdown timer

--- src/Launch.jsx
+++ src/Launch.jsx
@@ -1,10 +1,10 @@
 import React, { useState, useEffect } from 'react';
+import Timer from './Timer';
 
 export default function Launch() {
     const [error, setError] = useState(null);
     const [isLoaded, setIsLoaded] = useState(false);
     const [nextLaunch, setNextLaunch] = useState({});
-    // const [timeToLaunch, setTimeToLaunch] = useState(0);
 
     useEffect(() => {
         fetch('https://api.spacexdata.com/v4/launches/next')
@@ -31,11 +31,18 @@ export default function Launch() {
     } if (!isLoaded) {
         return <div>Loading...</div>;
     }
+
+    let img;
+    if (nextLaunch.links.patch.small !== null) {
+        img = <img src={nextLaunch.links.patch.small} alt={`${nextLaunch.name} mission patch`} />;
+    }
+
     return (
         <div>
             <h1>{nextLaunch.name}</h1>
-            <img src={nextLaunch.links.patch.small} alt={`${nextLaunch.name} mission patch`} />
-            <h2>{nextLaunch.details}</h2>
+            {img}
+            <Timer futureDate={Date.parse(nextLaunch.date_utc)} />
+            <p>{nextLaunch.details}</p>
         </div>
     );
 }
---
+++ src/Timer.jsx
@@ -0,0 +1,36 @@
+import React, { useState, useEffect } from 'react';
+
+const dayMs = 1000 * 60 * 60 * 24;
+const hourMs = 1000 * 60 * 60;
+const minMs = 1000 * 60;
+const secMs = 1000;
+
+function howFarFromNow(futureDate) {
+    const diffTime = futureDate - Date.now();
+    return {
+        days: Math.floor(diffTime / dayMs),
+        hours: Math.floor(diffTime / hourMs) % 24,
+        mins: Math.floor(diffTime / minMs) % 60,
+        secs: Math.floor(diffTime / secMs) % 60,
+    };
+}
+
+export default function Timer(props) {
+    const [time, setTime] = useState({
+        days: 0, hours: 0, mins: 0, secs: 0,
+    });
+
+    useEffect(() => {
+        // Do it immediately and also every second.
+        setTime(howFarFromNow(props.futureDate));
+        setInterval(() => {
+            setTime(howFarFromNow(props.futureDate));
+        }, 1000);
+    }, []);
+
+    return (
+        <p>
+            {`${time.days} days : ${time.hours} hours : ${time.mins} minutes : ${time.secs} seconds`}
+        </p>
+    );
+}
