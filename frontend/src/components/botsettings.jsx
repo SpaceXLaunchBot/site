ea7659c2364f331c73f713cd652f46df7de87057
thatguywiththatname <simjenner3@gmail.com>
2020-12-03 05:46:57 +0000

    Fix token storage and passing to channel components

--- frontend/src/components/botsettings.jsx
+++ frontend/src/components/botsettings.jsx
@@ -6,29 +6,36 @@ import Guild from './guild';
 import GetGuildsWithSubscribed from '../internalapi/guildswithsubscribed';
 
 export default function BotSettings() {
+    // TODO: Is localStorage insecure?
+    const [discordOAuthToken, setDiscordOAuthToken] = useState(localStorage.getItem('discord-oauth-token') || '');
     const [loggedIn, setLoggedIn] = useState(false);
     const [loaded, setLoaded] = useState(false);
     const [subscribedInfo, setSubscribedInfo] = useState({});
 
     useEffect(() => {
-        // TODO: Is localStorage insecure?
-        // TODO: What happens if token is expired or incorrect?
-        let accessToken = localStorage.getItem('discord-oauth-token') || '';
-        if (accessToken === '') {
+        // We use a local var here as the set state functions are async and don't change the state
+        // variable straight away.
+        let localToken = discordOAuthToken;
+
+        if (localToken === '') {
             const fragment = new URLSearchParams(window.location.hash.slice(1));
             if (fragment.has('access_token')) {
                 window.history.pushState(null, document.title, '/');
-                accessToken = fragment.get('access_token');
-                localStorage.setItem('discord-oauth-token', accessToken);
+                localToken = fragment.get('access_token');
+                localStorage.setItem('discord-oauth-token', localToken);
+                setDiscordOAuthToken(localToken);
             }
         }
-        // looks like it should be else, but we want to this to trigger even if accessToken was ''.
-        if (accessToken !== '') {
+
+        // If we either already have or just discovered an access token.
+        if (localToken !== '') {
             setLoggedIn(true);
             // Effects run asynchronously away from the actual render, so this will re-render
             // when setLoggedIn gets called above and the below web request will still be
             // happening in the background.
-            GetGuildsWithSubscribed(accessToken)
+            // TODO: What happens if token is expired or incorrect?
+            // TODO: What happens if API returns error?
+            GetGuildsWithSubscribed(localToken)
                 .then((json) => {
                     setSubscribedInfo(json);
                     setLoaded(true);
@@ -44,12 +51,13 @@ export default function BotSettings() {
     }
 
     // The IDs are used as keys, just because they are there and are unique.
-    // TODO: What happens if API returns error?
     const guilds = [];
     for (const guildId in subscribedInfo) {
         const subbedChannelsElems = [];
         for (const channel of subscribedInfo[guildId].subscribed_channels) {
-            subbedChannelsElems.push(<Channel key={channel.id} info={channel} />);
+            subbedChannelsElems.push(
+                <Channel key={channel.id} info={channel} discordOAuthToken={discordOAuthToken} />,
+            );
         }
 
         const guildInfo = subscribedInfo[guildId];
