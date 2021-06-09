9d153d47582da357ab490d40bf3d23f7a67ecda4
Simon <14183909+psidex@users.noreply.github.com>
2021-06-10 00:15:00 +0100

    simonj.tech -> simonj.dev

--- frontend/src/components/login.jsx
+++ frontend/src/components/login.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Button } from '@material-ui/core';
 
-let oauthUrl = 'https://discord.com/api/oauth2/authorize?client_id=782810710546579476&redirect_uri=https%3A%2F%2Fspacexlaunchbot.simonj.tech%2F&response_type=token&scope=identify%20guilds';
+let oauthUrl = 'https://discord.com/api/oauth2/authorize?client_id=782810710546579476&redirect_uri=https%3A%2F%2Fslb.simonj.dev%2F&response_type=token&scope=identify%20guilds';
 if (window.location.hostname === 'localhost') {
   oauthUrl = 'https://discord.com/api/oauth2/authorize?client_id=782810710546579476&redirect_uri=http%3A%2F%2Flocalhost%3A8080&response_type=token&scope=identify%20guilds';
 }
