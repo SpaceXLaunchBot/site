e69c56b92d39d39516ae490c6e4ab38d64d5059b
thatguywiththatname <simjenner3@gmail.com>
2021-07-09 22:55:49 +0100

    Set header in metrics endpoint

--- internal/api/metrics.go
+++ internal/api/metrics.go
@@ -3,5 +3,6 @@ package api
 import "net/http"
 
 func (a Api) Metrics(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
 	endWithResponse(w, genericResponse{Error: "endpoint not implemented", StatusCode: http.StatusNotImplemented})
 }
